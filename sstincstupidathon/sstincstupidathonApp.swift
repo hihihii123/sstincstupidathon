eb31c83a4c921b0996b42871b7736c123355b98a
Ang Sen Luen Matthias <ang_sen_luen_matthias@s2023.ssts.edu.sg>
2023-11-22 09:50:05 +0800

    Initial Commit

---
+++ sstincstupidathon/sstincstupidathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  sstincstupidathonApp.swift
+//  sstincstupidathon
+//
+//  Created by Ang Sen Luen Matthias on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct sstincstupidathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
